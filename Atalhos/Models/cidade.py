3eca6f9558918a97db4db26a813bded25b234681
RuiDevin <rafaelalvaro00@gmail.com>
2023-11-09 10:37:48 -0300

    chave estrangeira estadfo e cidade

--- Atalhos/Models/cidade.py
+++ Atalhos/Models/cidade.py
@@ -1,4 +1,8 @@
 from django.db import models
 
+from .zona_da_cidade import ZonaCidade
+
 class Cidade(models.Model):
-    nome_cidade = models.CharField(max_length=255)
+    nome_do_estado = models.CharField(max_length=255)
+    parte_cidade = models.ForeignKey(ZonaCidade, on_delete=models.PROTECT, null=True) 
+    
--- Atalhos/Models/estado.py
+++ Atalhos/Models/estado.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from .zona_da_estado import ZonaEstado
+
 class Estado(models.Model):
     nome_do_estado = models.CharField(max_length=255)
+    parte_estado = models.ForeignKey(ZonaEstado, on_delete=models.PROTECT, null=True) 
     
